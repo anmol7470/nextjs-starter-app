008056ed07977dcbf71817b243d50ba2fe3dfa33
anmol7470 <238388601+anmol7470@users.noreply.github.com>
2025-10-17 12:19:24 -0700

    starter is ready

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import '@/lib/env/client'
+import '@/lib/env/server'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  devIndicators: false,
+}
 
-export default nextConfig;
+export default nextConfig
