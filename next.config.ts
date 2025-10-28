20f326ff68349099a7fdca467b0c72b1e41e3c00
anmol7470 <238388601+anmol7470@users.noreply.github.com>
2025-10-28 15:21:30 -0700

    improve scripts and next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import '@/lib/env/client'
 import '@/lib/env/server'
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  devIndicators: false,
+  reactCompiler: true,
+}
 
 export default nextConfig
